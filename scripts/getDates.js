9aa31deac3060af82bff918a304767d0d4acd9f6
P10smeyer <preston.tensmeyer@gmail.com>
2024-01-11 12:14:30 -0700

    W01 Activity: Course Home Page Development. Completed mobile scrren view.

---
+++ scripts/getDates.js
@@ -0,0 +1,4 @@
+const options = {year: 'numeric'};
+document.getElementById('yearFooter').textContent = new Date().toLocaleDateString('en-US', options);
+const dateModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = dateModified;
