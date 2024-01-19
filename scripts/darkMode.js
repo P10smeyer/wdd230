b036dcbef98d2e93e4678b2c0aa933e2f1c9e022
P10smeyer <preston.tensmeyer@gmail.com>
2024-01-19 15:06:45 -0700

    Implemented dark mode to home page.

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,31 @@
-const myBtn = document.querySelector("#dark-mode");
+const myBtn = document.querySelector(".dark-mode");
+const darkLogo = document.querySelector('#dark-logo')
 const main = document.querySelector("main");
+const mainHeader = document.querySelector(".main-header");
 
 myBtn.addEventListener('click', () => {
-    if (myBtn.textContent = "🕶️") {
-        myBtn.textContent = "🔆";
+    if (darkLogo.textContent == "🕶️") {
+        darkLogo.textContent = "🔆";
+        main.classList.toggle('dark');
+        // mainHeader.classList.toggle('dark');
     }
     else {
-        myBtn.textContent = "🕶️"
+        darkLogo.textContent = "🕶️";
         main.classList.toggle('dark');
     }
-	// main.classList.toggle('dark');
-	// myBtn.classList.toggle('dark');
+	myBtn.classList.toggle('dark');
 });
+
+// myBtn.addEventListener('click', () => {
+//     if (myBtn.textContent = "🕶️") {
+//         myBtn.textContent = "🔆";
+//     }
+//     else {
+//         myBtn.textContent = "🕶️"
+//         // main.classList.toggle('dark');
+//     }
+// 	// main.classList.toggle('dark');
+// 	myBtn.classList.toggle('dark');
+// });
+
+// navElement.classList.toggle('open');
