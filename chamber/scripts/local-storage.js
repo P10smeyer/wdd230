40d60dee20633b13dac7beb5d90c5a8f668c47f8
P10smeyer <preston.tensmeyer@gmail.com>
2024-01-29 16:38:20 -0700

    Put the message on a separate line of the page visits.

--- chamber/scripts/local-storage.js
+++ chamber/scripts/local-storage.js
@@ -1,10 +1,13 @@
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let firstVisit = document.querySelector(".firstVisit");
 
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+    numVisits = 1;
+    visitsDisplay.textContent = numVisits;
+	firstVisit.textContent = `Welcome! Let us know if you have any questions.`;
 }
 
 numVisits++;
