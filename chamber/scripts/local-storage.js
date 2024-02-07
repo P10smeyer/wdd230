8b9a79a01ef32dcb54b03e0b5c239df2e49ba015
P10smeyer <preston.tensmeyer@gmail.com>
2024-02-06 22:38:28 -0700

    Updates to W03 Chamber Discover.

--- chamber/scripts/local-storage.js
+++ chamber/scripts/local-storage.js
@@ -1,14 +1,30 @@
 const visitsDisplay = document.querySelector(".visits");
+const msToDays = 86400000;
+const date = Date.now();
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let pastDate = Number(window.localStorage.getItem("pastDate-ls")) || date;
 let firstVisit = document.querySelector(".firstVisit");
+let backSoSoon = document.querySelector(".backSoSoon");
+let lastVisited = document.querySelector(".lastVisited");
 
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
-} else {
+} 
+else {
     numVisits = 1;
     visitsDisplay.textContent = numVisits;
 	firstVisit.textContent = `Welcome! Let us know if you have any questions.`;
 }
 
+if ((date - pastDate) > msToDays) {
+    backSoSoon.textContent = `Back so soon! Awesome!`;
+}
+else {
+    backSoSoon.textContent = "";
+    let days = (date - pastDate) / msToDays;
+    lastVisited.textContent = `You last visited ${days.toFixed(0)} days ago.`;
+}
+
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
+localStorage.setItem("pastDate-ls", pastDate);
