9cf66d4d3a33c3ce297ba890b0648f443efd454a
P10smeyer <preston.tensmeyer@gmail.com>
2024-02-07 16:34:38 -0700

    Initial submit for W05 Team activity.

---
+++ scripts/links.js
@@ -0,0 +1,20 @@
+const baseURL = "https://P10smeyer.github.io/wdd230/";
+const linksURL = "https://P10smeyer.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+getLinks();
